7d0fbbe0cad6a748b667c9e09b2a40850f971fb4
Dorian Fabre <Dorian.Fabre@bottomline.com>
2024-01-23 16:50:28 +0000

    randomise production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'; // used for customising CSS in React
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+// In production, generated CSS names are changed and shortened to save download time (jss1, jss2, jss3...)
+// As a result, every app used in the microfrontend will have the same CSS identifiers applied to different elements
+// To prevent this, use a production prefix:
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   )
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'; // used for customising CSS in React
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// In production, generated CSS names are changed and shortened to save download time (jss1, jss2, jss3...)
+// As a result, every app used in the microfrontend will have the same CSS identifiers applied to different elements
+// To prevent this, use a production prefix:
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default () => {
   return <div>
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route
